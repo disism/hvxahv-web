731dcbc3d1299705da65c8428d148441a615f57c
HVTURINGGA <x@disism.com>
2021-02-12 23:29:54 +0800

    add folower and following

---
+++ components/follow/follower.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+const Follower = () => {
+  const [follower, setFollower] = useState({})
+  useEffect(() => {
+    const token = localStorage.getItem("godis_login_token")
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("http://localhost:7000/api/v1/follower", requestOptions)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        setFollower(res)
+      })
+      .catch(err => console.log('error', err));
+  }, [])
+
+  return (
+    <div>
+      {follower &&
+        <div>
+          <div>被关注数：{follower.count}</div>
+          <div>
+            {follower.res && follower.res.map((item, idx) => {
+              return (
+                <div key={idx}>
+                  {item}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default Follower
